d3091b03579d5100e1073ef834d24b3b25edde58
zhangcheng <zh.milo@gmail.com>
2024-11-25 21:24:03 +0800

    remove unusage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Search, Loader2 } from 'lucide-react';
 import { Asset } from './types';
 import { AssetCard } from './components/AssetCard';
